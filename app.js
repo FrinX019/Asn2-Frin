1a573ca2f2700ef9b75df008e1eae604a971e336
frinx019 <frinpatel1841974@gmail.com>
2025-10-29 20:20:45 -0400

    Added views path for Vercel

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+/******************************************************************************
+***
+* ITE5315 – Assignment 2
+* I declare that this assignment is my own work in accordance with Humber Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Frin Sureshbhai Patel Student ID: N01680041 Date: 2025-10-29
+*
+*
+******************************************************************************
+**/
+
+
 // import express module
 var express = require('express');
 var path = require('path');
